82dc6394dd3cbd56a85431a47cd334d41e827544
mys12 <mys62811@naver.com>
2019-11-05 15:47:21 +0900

    connect UI

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.get('/', function (req,res){
   res.render('index');
 });
 
+app.get('/portfolio-4-col', function (req, res){
+  res.render('petlist');
+});
+
+app.get('/portfolio-item', function (req, res){
+  res.render('detail');
+});
 
 app.listen(3000, function(){
   console.log('Server On!');
