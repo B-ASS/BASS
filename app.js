dd8c8f46c73b441b7b8ebe27a6c577622a3a78d5
mys12 <mys62811@naver.com>
2019-11-12 10:51:44 +0900

    connect db

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const config = require('./config/config');
-const tokenAbi = require('./config/erc20ABI')
-const request = require('request');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var config = require('./config/config');
+var tokenAbi = require('./config/erc20ABI');
+var request = require('request');
 
 /*
 // init web3
@@ -13,7 +13,26 @@ const Web3 = require('web3');
 const web3 = new Web3(config.getConfig().httpEndpoint);
 */
 
-let app = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var app = express();
+
+
+// DB setting
+mongoose.set('useNewUrlParser', true);    // 1
+mongoose.set('useFindAndModify', false);  // 1
+mongoose.set('useCreateIndex', true);     // 1
+mongoose.connect("mongodb+srv://pets:qmffhrcpdls@cluster0-b7hly.mongodb.net/test?retryWrites=true&w=majority"); // 2
+var db = mongoose.connection; // 3
+// 4
+db.once("open", function(){
+  console.log("DB connected");
+});
+// 5
+db.on("error", function(err){
+  console.log("DB ERROR : ", err);
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,43 +43,65 @@ app.use(express.static(path.join(__dirname, '/public')));
 //app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 
+app.use(bodyParser.json()); // 2
+app.use(bodyParser.urlencoded({extended:true})); // 3
+
+// DB schema // 4
+var loginSchema = mongoose.Schema({
+  account:{type:String, required:true, unique:true},
+  password:{type:String, required:true}
+});
+
+var Login = mongoose.model("login", loginSchema);
+
 let token_list = []
 let address
 let privateKey
 
 // login
-app.use('/login', function(req, res) {
-  const method = req.method;
+// app.use('/login', function(req, res) {
+//   var method = req.method;
+//
+//   if (method == 'GET') {
+//     res.render('login');
+//   } else {
+//     privateKey = req.param('private_key');
+//     address = req.param('address');
+//     var globalConfig = config.getConfig();
+//     globalConfig.privateKey = privateKey;
+//     globalConfig.address = address;
+//
+//     res.redirect('/');
+//   }
+// });
 
-  if (method == 'GET') {
-    res.render('login');
-  } else {
-    privateKey = req.param('private_key');
-    address = req.param('address');
-    const globalConfig = config.getConfig();
-    globalConfig.privateKey = privateKey;
-    globalConfig.address = address;
-
-    res.redirect('/');
-  }
+app.get('/login', function(req, res){
+  res.render('login');
+});
+// Contacts - create // 9
+app.post('/login', function(req, res){
+  Login.create(req.body, function(err, signin){
+    if(err) return res.json(err);
+    res.redirect('login');
+  });
 });
 
 app.get('/', function(req, res) {
    res.render('index');
  });
- 
+
  app.get('/about', function(req, res) {
    res.render('about');
  });
- 
+
  app.get('/petlist', function (req, res){
 	res.render('petlist');
   });
-  
+
   app.get('/detail', function (req, res){
 	res.render('detail');
   });
- 
+
  app.get('/register', function(req, res) {
    res.render('register');
  });
